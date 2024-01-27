cba554b1214b3567de1819ee6bdf2c6145cb77f1
orlovanton01 <aov-perm@mail.ru>
2024-01-27 18:28:50 +0500

    Добавил и активировал модели, а также зарегал админа с логином и паролем admin

--- aggregator/admin.py
+++ aggregator/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Course, User, Review, Favorite
+
+admin.site.register(Course)
+admin.site.register(User)
+admin.site.register(Review)
+admin.site.register(Favorite)
--- aggregator/models.py
+++ aggregator/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    owner=models.CharField(max_length=50)
+    course_name=models.CharField(max_length=50)
+    price=models.FloatField
+    final_rating=models.DecimalField(decimal_places=1, max_digits=2)
+    link=models.TextField
+
+class User(models.Model):
+    last_name=models.CharField(max_length=50)
+    first_name=models.CharField(max_length=50)
+    patronymic=models.TextField
+    email=models.CharField(max_length=80)
+    password=models.CharField(max_length=80)
+
+class Review(models.Model):
+    text_review=models.TextField
+    rating=models.IntegerField
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    course=models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Favorite(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    course=models.ForeignKey(Course, on_delete=models.CASCADE)
+
