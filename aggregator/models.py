ea34c768135ef15710c50b5d257603a5ce78767f
orlovanton01 <aov-perm@mail.ru>
2024-01-28 16:41:10 +0500

    Добавил недостающие поля в модели и заполнил таблицу Курсы в БД тестовыми данными

--- aggregator/models.py
+++ aggregator/models.py
@@ -5,20 +5,20 @@ from django.db import models
 class Course(models.Model):
     owner=models.CharField(max_length=50)
     course_name=models.CharField(max_length=50)
-    price=models.FloatField
-    final_rating=models.DecimalField(decimal_places=1, max_digits=2)
-    link=models.TextField
+    price=models.FloatField(null=True)
+    final_rating=models.DecimalField(decimal_places=1, max_digits=2, null=True)
+    link=models.TextField(null=True)
 
 class User(models.Model):
     last_name=models.CharField(max_length=50)
     first_name=models.CharField(max_length=50)
-    patronymic=models.TextField
-    email=models.CharField(max_length=80)
+    patronymic=models.TextField(null=True)
+    email=models.EmailField()
     password=models.CharField(max_length=80)
 
 class Review(models.Model):
-    text_review=models.TextField
-    rating=models.IntegerField
+    text_review=models.TextField(null=True)
+    rating=models.IntegerField(null=True)
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     course=models.ForeignKey(Course, on_delete=models.CASCADE)
 
