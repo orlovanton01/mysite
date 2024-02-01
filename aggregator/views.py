a6ad5d217d8634701218a7274e87599845ed1833
orlovanton01 <aov-perm@mail.ru>
2024-02-01 17:23:28 +0500

    Добавил обработку ошибки 404

--- aggregator/views.py
+++ aggregator/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Course
 
@@ -6,3 +6,7 @@ def index(request):
     latest_courses_list=Course.objects.all()
     context = {"latest_courses_list": latest_courses_list}
     return render(request, "aggregator/index.html", context)
+
+def detail(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, "aggregator/detail.html", {"course": course})
