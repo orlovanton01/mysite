fa485a4b50e4b7976b5e44236b8248b9718218e9
orlovanton01 <aov-perm@mail.ru>
2024-02-16 16:59:54 +0500

    Переопределил методы str

--- aggregator/models.py
+++ aggregator/models.py
@@ -10,6 +10,9 @@ class Course(models.Model):
     link=models.TextField(default='')
     training_period=models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.course_name
+
 class User(models.Model):
     last_name=models.CharField(max_length=50)
     first_name=models.CharField(max_length=50)
@@ -17,13 +20,22 @@ class User(models.Model):
     email=models.EmailField()
     password=models.CharField(max_length=80)
 
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} {self.patronymic}'
+
 class Review(models.Model):
     text_review=models.TextField(default='')
     rating=models.DecimalField(decimal_places=1, max_digits=2, null=True, verbose_name='Рейтинг человека')
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     course=models.ForeignKey(Course, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.text_review
+
 class Favorite(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     course=models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} {self.course}'
 
