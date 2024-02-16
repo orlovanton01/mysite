2a211e62c68cb3e8a2b3d998332e9e422f9a5a4b
orlovanton01 <aov-perm@mail.ru>
2024-02-16 16:50:17 +0500

    Изменил url главной страницы

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("aggregator/", include("aggregator.urls")),
+    path("", include("aggregator.urls")),
     path('admin/', admin.site.urls),
 ]
