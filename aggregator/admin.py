84fb36c02448058aae919c93caeffcfbe3386a3a
orlovanton01 <aov-perm@mail.ru>
2024-02-16 19:01:12 +0500

    Расширил модель пользователя с помощью связи один-к-одному

--- aggregator/admin.py
+++ aggregator/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Course, User, Review, Favorite
+from .models import Course, Profile, Review, Favorite
 
 admin.site.register(Course)
-admin.site.register(User)
+admin.site.register(Profile)
 admin.site.register(Review)
 admin.site.register(Favorite)
--- aggregator/models.py
+++ aggregator/models.py
@@ -1,39 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
 class Course(models.Model):
     owner=models.CharField(max_length=50)
     course_name=models.CharField(max_length=50)
-    price=models.DecimalField(decimal_places=2, max_digits=8, default=0)
-    final_rating=models.DecimalField(decimal_places=1, max_digits=2, null=True, verbose_name='Рейтинг курса')
-    link=models.TextField(default='')
-    training_period=models.IntegerField(default=0)
+    price=models.FloatField(null=True)
+    final_rating=models.DecimalField(decimal_places=1, max_digits=2, null=True)
+    link=models.TextField(null=True)
+    training_period=models.IntegerField(null=True)
 
-    def __str__(self):
-        return self.course_name
-
-class User(models.Model):
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     last_name=models.CharField(max_length=50)
     first_name=models.CharField(max_length=50)
     patronymic=models.TextField(null=True)
     email=models.EmailField()
-    password=models.CharField(max_length=80)
-
+    
     def __str__(self):
         return f'{self.last_name} {self.first_name} {self.patronymic}'
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 class Review(models.Model):
     text_review=models.TextField(default='')
     rating=models.DecimalField(decimal_places=1, max_digits=2, null=True, verbose_name='Рейтинг человека')
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    user=models.ForeignKey(Profile, on_delete=models.CASCADE)
     course=models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text_review
 
 class Favorite(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    user=models.ForeignKey(Profile, on_delete=models.CASCADE)
     course=models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
