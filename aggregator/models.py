b1678d8c018de5ff8692ec47e9285dbefba29e0d
orlovanton01 <aov-perm@mail.ru>
2024-02-19 09:48:51 +0500

    Добавил исключение если поля в Profile пустые

--- aggregator/models.py
+++ aggregator/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your models here.
 
@@ -28,6 +29,10 @@ class Profile(models.Model):
     
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
+        try:
+            instance.profile.save()
+        except ObjectDoesNotExist:
+            Profile.objects.create(user=instance)
         if created:
             Profile.objects.create(user=instance)
 
